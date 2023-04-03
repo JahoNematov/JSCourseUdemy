20899b1b8ccbf2c8512020011ae5ad7fcb2ec292
Jahongir <j.sh.nematov@gmail.com>
2023-04-03 22:37:57 +0500

    Working with mobile device events

---
+++ 49. Mobile Device Events/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// There are 6 events on mobile devices
+
+// touchstart -> event when you touch (simple touch)
+// touchmove -> when you put finger and move it
+// touchend -> when you release finger
+// touchenter -> when you slide-in to element
+// touchleave -> when you slide-of from element
+// touchcancel -> when your finger gets out from browser scope
+
+window.addEventListener("DOMContentLoaded", () => {
+    const box = document.querySelector(".box");
+
+    box.addEventListener('touchstart', (event) => {
+        event.preventDefault();
+        
+        console.log(event.touches);
+        console.log("The button is touched.");
+    })
+
+    box.addEventListener('touchmove', (event) => {
+        event.preventDefault();
+        console.log("Touch move!");
+    })
+
+    box.addEventListener("touchend", (event) => {
+        event.preventDefault();
+        console.log("Touch end");
+    })
+
+})
+
+// Three main attributes of touch events
+// touches -> number of fingers involved in event + their params
+// targetTouches -> number of fingers that touch exactly this element
